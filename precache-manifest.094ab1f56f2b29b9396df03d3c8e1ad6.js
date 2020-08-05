d75cfaff3aca54c1e0eff4500908442554a06e7e
Paris Schuette <parisschuette@gmail.com>
2020-08-05 18:11:56 -0500

    Updates

---
+++ precache-manifest.094ab1f56f2b29b9396df03d3c8e1ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5b1f2d3a8802264d4cf14242dc65e1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "91e20c3043a91c8fa18f",
+    "url": "/portfolio/static/css/main.37742ff9.chunk.css"
+  },
+  {
+    "revision": "ea4eca82e61acb7d425f",
+    "url": "/portfolio/static/js/2.7fc90c72.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.7fc90c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e20c3043a91c8fa18f",
+    "url": "/portfolio/static/js/main.571a44ef.chunk.js"
+  },
+  {
+    "revision": "ea39b8ee9df3c2b4c2bb",
+    "url": "/portfolio/static/js/runtime-main.12f1bd73.js"
+  }
+]);
