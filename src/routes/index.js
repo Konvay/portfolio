8e20583970767d71de79066e4151944d61cc59b3
Paris Schuette <parisschuette@gmail.com>
2020-08-05 18:27:26 -0500

    Updating route to match GitHub pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import NotFound from '../pages/NotFound'
 
+const rootRoute = "/portfolio";
+
 const routes = [
-  { path: "/", key: "ROOT", exact: true, component: Home },
-  { path: "/about", key: "ABOUT", exact: true, component: About },
+  { path: `${rootRoute}/`, key: "ROOT", exact: true, component: Home },
+  { path: `${rootRoute}/about`, key: "ABOUT", exact: true, component: About },
 
   // Catch All Else
-  { path: "/:NotFound", component: NotFound }
+  { path: `${rootRoute}/:NotFound`, component: NotFound }
 ];
 
 export default routes;
